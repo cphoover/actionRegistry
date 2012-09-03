8cb06d25162daa7ba8405bcb5520dc6f368451fc
Charlie Hoover <cphoover@gmail.com>
2012-09-03 15:06:55 -0400

    initial commit

---
+++ actionRegistry.js
@@ -0,0 +1,22 @@
+
+function AdgActionRegistry(){
+        this.actions = {};
+}
+AdgActionRegistry.prototype.register = function(trigger, action){
+ //if the index hasn't been initialized initialize it..
+ if(typeof this.actions[trigger] == 'undefined'){
+    this.actions[trigger] = new Array();
+ }
+//if paramater is a function just push it.
+ if(typeof action == "function"){
+        this.actions[trigger].push(action);
+ }
+ else if(action instanceof Array){
+    for(var i in action){
+        this.register(trigger, action[i]);
+    }
+ }
+ else{
+    throw "Invalid type passed to register method. " + trigger + " : " + action;
+ }
+}
